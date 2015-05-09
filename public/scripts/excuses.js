6ba9c65f2bc3cd069ca6dc4dda2319b0ec82c02f
Evan Griffiths <griffithse@gmail.com>
2015-05-09 14:13:57 -0400

    progress

---
+++ public/scripts/excuses.js
@@ -0,0 +1,129 @@
+angular.module('excuses', ['ngRoute','ngResource'])
+  .controller('MainController', function($window,$scope,$route,$http,$location) {
+    $scope.session = $window.sessionStorage;
+    $http.get('api/checksession')
+      .success(function() {
+      })
+      .error(function() {
+        $scope.logout();
+      });
+
+    $scope.logout = function logout() {
+      delete $window.sessionStorage.token;
+      $location.path('login');
+    }
+    $scope.newUser = function newUser() {
+      $location.path('users/new');
+    }
+  })
+
+  .controller('SetAlarmController', function($resource,$scope,$http,$routeParams) {
+    $http.get('api/user').success(function (data) {
+      $scope.user = data.user;
+    });
+    $http.get('api/excuses').success(function (data) {
+      $scope.excuses = data;
+      $scope.excuse = 1;
+    });
+    updateAlarms();
+
+    function updateAlarms() {
+      $http.get('api/alarms').success(function (data) {
+        $scope.alarms = data;
+      });
+    }
+
+    $scope.setAlarm = function setAlarm() {
+      $http.post('api/alarms', {
+        time: new Date($scope.time).getTime(),
+        excuse_id: $scope.excuse
+      }).success(updateAlarms);
+    };
+
+    $scope.cancelAlarm = function cancelAlarm(id) {
+      $http.delete('api/alarm/'+id).success(updateAlarms);
+    };
+  })
+
+  .controller('LoginController', function($scope,$http,$window,$location) {
+    $scope.login = function() {
+      $http.post('/authenticate', {email:$scope.email, password:$scope.password})
+        .success(function (data,status,headers,config) {
+          $window.sessionStorage.token = data.token;
+          $location.path('/user');
+        })
+        .error(function (data,status,headers,config) {
+          delete $window.sessionStorage.token;
+          alert(data);
+          // alert("Error: Unknown email/password combination");
+        });
+    };
+  })
+
+  .controller('NewUserController', function($scope, $http, $location) {
+    $scope.newUser = function() {
+      // console.log({
+      //   name: $scope.name,
+      //   phone: $scope.phone,
+      //   email: $scope.email,
+      //   password: $scope.password
+      // });
+      $http.post('/users', {
+        name: $scope.name,
+        phone: $scope.phone,
+        email: $scope.email,
+        password: $scope.password
+      })
+        .success(function() {
+          alert('yay!');
+          // $http.post('/authenticate', {email:$scope.email, password:$scope.password})
+          //   .success(function (data,status,headers,config) {
+          //     $window.sessionStorage.token = data.token;
+          //     $location.path('/user');
+          //   })
+          //   .error(function (data,status,headers,config) {
+          //     delete $window.sessionStorage.token;
+          //     alert("Error: Unknown email/password combination");
+          //   });
+        })
+        .error(function(err) {
+          alert(err);
+        });
+    }
+  })
+
+  .factory('authInterceptor', function($rootScope, $q, $window, $location) {
+    return {
+      request: function(config) {
+        config.headers = config.headers || {};
+        if ($window.sessionStorage.token) {
+          config.headers.Authorization = 'Bearer ' + $window.sessionStorage.token;
+        }
+        return config;
+      },
+      response: function(response) {
+        if (response.status === 401) {
+          delete $window.sessionStorage.token;
+          $location.path('login');
+        }
+        return response || $q.when(response);
+      }
+    };
+  })
+
+  .config(function($routeProvider, $httpProvider) {
+    $httpProvider.interceptors.push('authInterceptor');
+    $routeProvider
+      .when('/login', {
+        templateUrl: 'templates/login.html',
+        controller: 'LoginController'
+      })
+      .when('/user', {
+        templateUrl: 'templates/set-alarm.html',
+        controller: 'SetAlarmController'
+      })
+      .when('/users/new', {
+        templateUrl: 'templates/newuser.html',
+        controller: 'NewUserController'
+      });
+    });
---
+++ server.js
@@ -0,0 +1,113 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var sqlite3 = require('sqlite3');
+var expressJwt = require('express-jwt');
+var jwt = require('jsonwebtoken');
+var forge = require('node-forge');
+
+var db = new sqlite3.Database('db/excuses.db');
+var secrets = require('./secrets.json');
+
+var app = express();
+
+// any route prefixed with api will be authenticated
+app.use('/api',expressJwt({secret:secrets.jwt}));
+app.use(bodyParser.json({extended: false}));
+app.use(express.static('public'));
+
+
+app.get('/api/excuses', function(req,res) {
+  db.all("SELECT * FROM excuses", function(err,data) {
+    if(err) throw(err);
+    res.json(data);
+  });
+});
+
+app.get('/api/alarms', function(req,res) {
+  db.all("SELECT alarms.id,time,excuses.title,excuses.message FROM alarms JOIN excuses ON alarms.excuse_id = excuses.id WHERE user_id = ?", req.user.id, function(err,data) {
+    if(err) throw(err);
+    res.json(data);
+  });
+});
+
+app.post('/api/alarms', function(req,res) {
+  db.run("INSERT INTO alarms (time,excuse_id,user_id) VALUES (?,?,?)", req.body.time, req.body.excuse_id, req.user.id, function(err) {
+    if (err) throw(err);
+    res.end();
+    // db.get("SELECT alarms.id,time,excuses.title,excuses.message FROM alarms JOIN excuses ON alarms.excuse_id = excuses.id WHERE alarms.ROWID = ?", this.lastID, function(err,data) {
+    //   if (err) throw(err);
+    //   res.json(data);
+    // });
+  });
+});
+
+app.delete('/api/alarm/:id', function(req,res) {
+  db.run("DELETE FROM alarms WHERE id = ?", req.params.id, function(err) {
+    if (err) throw(err);
+    res.end();
+  });
+});
+
+app.post('/authenticate', function(req,res) {
+  db.get("SELECT * FROM users WHERE email = ?", req.body.email, function(err,data) {
+    if (err) throw(err);
+    if (typeof data === 'undefined') {
+      res.status(401).send('User doesn\'t exist');
+    } else {
+      var md = forge.md.sha256.create();
+      if (data.password === md.update(req.body.password + data.salt).digest().toHex()) {
+        //passwords match
+        var profile = {
+          name: data.name,
+          email: data.email,
+          phone: data.phone,
+          id: data.id
+        }
+        var token = jwt.sign(profile,secrets.jwt, {expiresInMinutes:60*5});
+        res.json({token:token});
+      } else {
+        //passwords don't match
+        res.status(401).send('Wrong password');
+      }
+    }
+  });
+});
+
+app.get('/api/checksession', function(req,res) {
+  res.json(true);
+});
+
+app.get('/', function(req,res) {
+  res.render('index.html');
+});
+
+app.get('/api/user', function(req,res) {
+  db.get("SELECT name,email,phone FROM users WHERE id = ?", req.user.id, function(err,data) {
+    if (err) throw(err);
+    res.json({user: data});
+  });
+});
+
+app.post('/users', function(req,res) {
+  //ensure nobody else has that email
+  db.get("SELECT email FROM users WHERE email = ?", req.body.email, function(err,data) {
+    if (typeof data !== 'undefined') {
+      res.status(401).send("Account already exists");
+    } else {
+      //email is free so proceed
+      var md = forge.md.sha256.create();
+      var salt = forge.random.getBytesSync(16);
+      md.update(req.body.password + salt);
+
+      db.run("INSERT INTO users (name,phone,email,password,salt) VALUES (?,?,?,?,?)", req.body.name, req.body.phone, req.body.email, md.digest().toHex(), salt, function(err) {
+        if (err) throw(err);
+        db.get("SELECT * FROM users WHERE ROWID = ?", this.lastID, function(err,data) {
+          if (err) throw(err);
+          res.json(data);
+        });
+      });
+    }
+  });
+});
+
+app.listen(3000);
